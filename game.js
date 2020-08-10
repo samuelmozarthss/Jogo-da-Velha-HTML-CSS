d7b8f976893ec50b194d1790db6fa7d8742c1968
Samuel Mozarth <contatosamuelm@gmail.com>
2020-08-10 17:54:35 -0300

    A proposta do jogo foi concluída mas ainda precisam de algumas modificações e melhorias

--- game.js
+++ game.js
@@ -1,6 +1,6 @@
-const player1   ="x";
-const player2   ="o";
-var playTime  = player1;
+const player1   ="X";
+const player2   ="O";
+var playTime    = player1;
 var gameOver    =false;
 
 atualizaMostrador();
@@ -35,22 +35,68 @@ function inicializarEspacos(){
 
                 if (playTime == player1) {
                     // this.innerHtml = "img src='<image/x.png' border='0' height='50'>"
-                    this.innerHtml = "img src='<image/x.png'>";
+                    this.innerHTML = "<img src='image/x.png' border='0' height='50'>";
                     this.setAttribute("jogada", player1);
                     playTime = player2;
 
                 }else{
 
-                    this.innerHtml = "img src='<image/o.png'>";
+                    this.innerHTML = "<img src='image/o.png' border='0' height='50'>";
                     this.setAttribute("jogada", player2);
                     playTime = player1;
 
             }
-
             atualizaMostrador();
+            verificarVencedor();
 
-        }
+
+
+            }
+        });
     }
-        );
-        
 }
+
+async function verificarVencedor(){
+
+var a1 = document.getElementById("a1").getAttribute("jogada");
+var a2 = document.getElementById("a2").getAttribute("jogada");
+var a3 = document.getElementById("a3").getAttribute("jogada");
+
+var b1 = document.getElementById("b1").getAttribute("jogada");
+var b2 = document.getElementById("b2").getAttribute("jogada");
+var b3 = document.getElementById("b3").getAttribute("jogada");
+
+var c1 = document.getElementById("c1").getAttribute("jogada");
+var c2 = document.getElementById("c2").getAttribute("jogada");
+var c3 = document.getElementById("c3").getAttribute("jogada");
+
+var vencedor = "";
+
+if((a1 == b1 && a1 == c1 && a1 != "") || (a1 == a2 && a1 == a3 && a1 != "") || (a1 == b2 && a1 == c3 && a1 != "")){
+    vencedor = a1;
+}else if
+  ((b2 == b1 && b2 == b3 && b2 != "") || (b2 == a2 && b2 == c2 && b2 != "") || (b2 == a3 && b2 == c1 && b2 != "")){
+    vencedor = b2;
+}else if
+  (((c3 == c2 && c3 == c1) || (c3 == a3 && c3 == b3)) && c3 != "")
+{
+    vencedor = c3;
+}
+
+if (vencedor != ""){
+    gameOver = true;
+
+    await sleep(50); 
+
+    alert("O ganhador foi o: '" + vencedor + "'")
+
+}
+}
+function sleep(ms){
+
+    return new Promise(resolve => setTimeout(resolve, ms));
+
+}
+
+
+
